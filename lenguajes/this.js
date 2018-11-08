e5a183bfd81ef703b382c69ae44a25f979a81808
alce65 <alce65@hotmail.es>
2018-11-08 12:01:26 +0100

    Lenguajes ES6 y TS

--- lenguajes/this.js
+++ lenguajes/this.js
@@ -4,7 +4,18 @@ let o = {
     nombre: 'Pepe',
     mostrar : function () {
         console.log(this)
+    },
+    saludar : function () {
+        // console.log('Hola '+ this.nombre)
+        console.log(`Hola 
+        ${this.nombre}`)
     }
 }
 
 o.mostrar()
+
+let saludo = o.saludar
+setTimeout(() => {
+    saludo()
+    // o.saludar()
+}, 2000);
