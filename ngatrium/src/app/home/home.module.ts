e104fca0f0e68efdfe0ffacf0cf0ec37c253120c
alce65 <alce65@hotmail.es>
2018-11-12 12:08:52 +0100

    Nuevos componentes saludo en version inicial

--- ngatrium/src/app/home/home.module.ts
+++ ngatrium/src/app/home/home.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { SaludoComponent } from './saludo/saludo.component';
+import { SaludoLocalComponent } from './saludo-local/saludo-local.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent, 
+    SaludoComponent,
+    SaludoLocalComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
---
+++ ngatrium/src/app/home/saludo-local/saludo-local.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'iam-saludo-local',
+  templateUrl: './saludo-local.component.html',
+  styleUrls: ['./saludo-local.component.css']
+})
+export class SaludoLocalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
