081982a120649144f679b13e421832d48d1d6d6a
alce65 <alce65@hotmail.es>
2018-11-13 11:08:45 +0100

    Componentes externos

--- ngatrium/src/app/app.module.ts
+++ ngatrium/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
+    NgbModule,
     AppRoutingModule,
     CoreModule
   ],
